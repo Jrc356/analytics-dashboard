1a03e4807dc71bd7ae198c52951be30dea3b9205
Jon Corbin <jon.corbin8@gmail.com>
2019-08-22 15:38:54 -0400

    Add metric data updating; Change style a bit

--- client/src/components/Dashboard/DashboardItem/DashboardItem.js
+++ client/src/components/Dashboard/DashboardItem/DashboardItem.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import { numberWithCommas } from "../../../utils";
 
 const styles = theme => ({
   paper: {
@@ -11,45 +12,95 @@ const styles = theme => ({
     padding: theme.spacing(10),
     textAlign: 'center',
     color: theme.palette.text.primary,
+    height: 100,
   },
   mainMetric: {
-    background: theme.palette.background.secondary,
+    background: theme.palette.background.quaternary,
   },
   secondaryMetric: {
-    background: theme.palette.background.tertiary,
+    background: theme.palette.background.secondary,
   },
+  defaultMetric: {
+    background: theme.palette.background.tertiary
+  }
 });
 
 class DashboardItem extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      classNames: this.props.classes,
+      size: this.props.size,
+      metric: this.props.metric,
+      priority: this.props.priority,
+      data: 'No data'
+    }
+  }
+
+  componentDidMount(){
+    this.getMetricData();
+    this.getClassNames();
+  }
+
+  getMetricData(){
+    const { metric } = this.state;
+    const strippedMetric = metric.replace(" ", "");
+    const url = `http://localhost:3001/api?metrics=${strippedMetric}`
+    fetch(url, {
+      method:'GET',
+      mode: 'cors',
+    })
+    .then((res) => {
+      return res.json()
+    }).then((data) => {
+      const value = parseInt(strippedMetric.startsWith('ga:') ? data.data[strippedMetric] : data.data[`ga:${strippedMetric}`]);
+      const formattedValue = numberWithCommas(value);
+      this.setState( { data: formattedValue } );
+    });
+  }
 
   getClassNames() {
-    const { priority, classes } = this.props
+    const { priority } = this.state;
+    const { classes } = this.props;
     let classNames = classes.paper; 
-    if (priority === 'primary'){
-      classNames = `${classNames} ${classes.mainMetric}`;
-    } else {
-      classNames = `${classNames} ${classes.secondaryMetric}`;
+    
+    switch(priority){
+      case 'primary':
+        classNames = `${classNames} ${classes.mainMetric}`;
+        break;
+      case 'secondary':
+        classNames = `${classNames} ${classes.secondaryMetric}`;
+        break;
+      default:
+        classNames = `${classNames} ${classes.defaultMetric}`;
+        break;
     }
-    return classNames;
+
+    this.setState({classNames});
   }
 
   render() {
-    const { size, metric } = this.props;
-    const classNames = this.getClassNames();
+    const 
+    { data, 
+      classNames, 
+      metric, 
+      size } = this.state;
 
-    // Create a graph component that takes metric as a prop and
-    // Turns it into some graph, probably add a graphType prop
     return (
-      <Grid item xs={ size }>
-        <Paper className={`${classNames}`}>{metric}</Paper>
+      <Grid item xs={ size ? size : 'auto' } zeroMinWidth height={100} width={100}>
+        <Paper className={`${classNames}`}>
+          <h2>{metric}</h2>
+          <p>{data}</p>
+        </Paper>
       </Grid>
     )
   }
 }
 
 DashboardItem.propTypes = {
-  size: PropTypes.number.isRequired,
-  priority: PropTypes.string.isRequired,
+  size: PropTypes.number,
+  priority: PropTypes.string,
 }
 
 export default withStyles(styles)(DashboardItem);
