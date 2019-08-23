7a868000378df45a71addb01ba3ac79b8ed3cc43
Jon Corbin <jon.corbin8@gmail.com>
2019-08-23 00:13:41 -0400

    convert to pure component and Fix eslint issues

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,26 +1,30 @@
-import React, { Component } from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import AppBar from '@material-ui/core/AppBar';
 
-const styles = theme => ({
+const styles = (theme) => ({
   header: {
     padding: theme.spacing(3),
     textAlign: 'center',
     color: theme.palette.text.primary,
     background: theme.palette.background.primary,
-  }
+  },
 });
 
-export class Header extends Component {
-  render() {
-    const { classes, text } = this.props;
-    return (
-      <AppBar position='static'>
-        <Paper className={classes.header}>{text}</Paper>
-      </AppBar>
-    )
-  }
-}
+export const Header = (props) => {
+  const { classes, text } = props;
+  return (
+    <AppBar position="static">
+      <Paper className={classes.header}>{text}</Paper>
+    </AppBar>
+  );
+};
+
+Header.propTypes = {
+  classes: PropTypes.object.isRequired,
+  text: PropTypes.string.isRequired,
+};
 
 export default withStyles(styles)(Header);
