d0ce0b5e302368659cd6d52edc15f53dc01fb4b8
Jon Corbin <jon.corbin8@gmail.com>
2019-08-22 23:51:57 -0400

    Change to pure component and fix eslint issues

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,34 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import DashboardItem from './DashboardItem/DashboardItem';
 
-const styles = theme => ({
+const styles = () => ({
   root: {
     flexGrow: 1,
-    overflow: "hidden",
+    overflow: 'hidden',
   },
 });
 
-class Dashboard extends Component {
-  render(){
-    const { classes } = this.props;
-    return (
-      <div className={classes.root}>
-        <Grid container spacing={3} justify={"center"} alignItems={"center"}>
-          <DashboardItem size={9} priority={'primary'} metric={'Users'} />
-          <DashboardItem size={3} priority={'secondary'} metric={'Sessions'} />
-          <DashboardItem size={3} priority={'primary'} metric={'Page Views'} />
-          <DashboardItem size={9} metric={'Transaction Revenue'} />
-        </Grid>
-      </div>
-    );
-  }
-}
+const Dashboard = (props) => {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3} justify="center" alignItems="center">
+        <DashboardItem size={9} priority="primary" metric="Users" />
+        <DashboardItem size={3} priority="secondary" metric="Sessions" visual="chart" type="line" />
+        <DashboardItem size={3} priority="primary" metric="Page Views" />
+        <DashboardItem size={9} metric="Transaction Revenue" />
+      </Grid>
+    </div>
+  );
+};
 
 Dashboard.propTypes = {
   classes: PropTypes.object.isRequired,
-}
+};
 
 export default withStyles(styles)(Dashboard);
