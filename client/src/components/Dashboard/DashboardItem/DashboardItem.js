e118f554aa270a49e791e151c9e7405ae45818b2
Jon Corbin <jon.corbin8@gmail.com>
2019-08-23 01:11:54 -0400

    Add option for different visualizations -- chart & text

--- client/src/components/Dashboard/DashboardItem/DashboardItem.js
+++ client/src/components/Dashboard/DashboardItem/DashboardItem.js
@@ -1,18 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import { numberWithCommas } from "../../../utils";
+import { TextItem, ChartItem } from '../DataItems';
+import { numberWithCommas, isMobile } from '../../../utils';
 
-const styles = theme => ({
+const styles = (theme) => ({
   paper: {
     marginLeft: theme.spacing(1),
     marginRight: theme.spacing(1),
-    padding: theme.spacing(10),
+    paddingTop: theme.spacing(10),
     textAlign: 'center',
     color: theme.palette.text.primary,
-    height: 100,
+    height: 200,
+    minWidth: 300,
+  },
+  chartItem: {
+    paddingTop: theme.spacing(1),
+    height: 272,
   },
   mainMetric: {
     background: theme.palette.background.quaternary,
@@ -21,51 +27,74 @@ const styles = theme => ({
     background: theme.palette.background.secondary,
   },
   defaultMetric: {
-    background: theme.palette.background.tertiary
-  }
+    background: theme.palette.background.tertiary,
+  },
 });
 
 class DashboardItem extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
+    const {
+      classes,
+      size,
+      metric,
+      priority,
+      visual,
+      type,
+    } = this.props;
+
     this.state = {
-      classNames: this.props.classes,
-      size: this.props.size,
-      metric: this.props.metric,
-      priority: this.props.priority,
-      data: 'No data'
-    }
+      classNames: classes,
+      size,
+      metric,
+      priority,
+      visual,
+      type,
+      data: 'No data',
+    };
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.getMetricData();
     this.getClassNames();
   }
 
-  getMetricData(){
-    const { metric } = this.state;
-    const strippedMetric = metric.replace(" ", "");
-    const url = `http://localhost:3001/api?metrics=${strippedMetric}`
+  getMetricData() {
+    const { visual, metric } = this.state;
+    const strippedMetric = metric.replace(' ', '');
+
+    let url;
+    if (visual === 'chart') {
+      url = `http://localhost:3001/api/graph?metric=${strippedMetric}`;
+    } else {
+      url = `http://localhost:3001/api?metrics=${strippedMetric}`;
+    }
     fetch(url, {
-      method:'GET',
+      method: 'GET',
       mode: 'cors',
     })
-    .then((res) => {
-      return res.json()
-    }).then((data) => {
-      const value = parseInt(strippedMetric.startsWith('ga:') ? data.data[strippedMetric] : data.data[`ga:${strippedMetric}`]);
-      const formattedValue = numberWithCommas(value);
-      this.setState( { data: formattedValue } );
-    });
+      .then((res) => (res.json()))
+      .then((data) => {
+        let value;
+        let formattedValue;
+        if (visual === 'chart') {
+          value = data.data[strippedMetric];
+          formattedValue = value;
+        } else {
+          value = strippedMetric.startsWith('ga:') ? data.data[strippedMetric] : data.data[`ga:${strippedMetric}`];
+          formattedValue = numberWithCommas(parseInt(value.value, 10));
+        }
+        this.setState({ data: formattedValue });
+      });
   }
 
   getClassNames() {
-    const { priority } = this.state;
+    const { priority, visual } = this.state;
     const { classes } = this.props;
-    let classNames = classes.paper; 
-    
-    switch(priority){
+    let classNames = classes.paper;
+
+    switch (priority) {
       case 'primary':
         classNames = `${classNames} ${classes.mainMetric}`;
         break;
@@ -77,30 +106,67 @@ class DashboardItem extends Component {
         break;
     }
 
-    this.setState({classNames});
+    if (visual === 'chart') {
+      classNames = `${classNames} ${classes.chartItem}`;
+    }
+
+    this.setState({ classNames });
+  }
+
+  getVisualComponent() {
+    const { data, visual, type } = this.state;
+
+    let component;
+    if (data === 'No data') {
+      component = <TextItem data={data} />;
+    } else {
+      switch (visual) {
+        case 'chart':
+          component = <ChartItem data={data} type={type} />;
+          break;
+        default:
+          component = <TextItem data={data} />;
+          break;
+      }
+    }
+
+    return component;
   }
 
   render() {
-    const 
-    { data, 
-      classNames, 
-      metric, 
-      size } = this.state;
+    const {
+      classNames,
+      metric,
+      size,
+    } = this.state;
+
+    const visualComponent = this.getVisualComponent();
 
     return (
-      <Grid item xs={ size ? size : 'auto' } zeroMinWidth>
+      <Grid item xs={(isMobile || !size) ? 'auto' : size} zeroMinWidth>
         <Paper className={`${classNames}`}>
-          <h2>{metric}</h2>
-          <p>{data}</p>
+          <h2>{ metric }</h2>
+          {visualComponent}
         </Paper>
       </Grid>
-    )
+    );
   }
 }
 
 DashboardItem.propTypes = {
   size: PropTypes.number,
   priority: PropTypes.string,
-}
+  visual: PropTypes.string,
+  type: PropTypes.string,
+  classes: PropTypes.object.isRequired,
+  metric: PropTypes.string.isRequired,
+};
+
+DashboardItem.defaultProps = {
+  size: null,
+  priority: null,
+  visual: 'text',
+  type: null,
+};
 
 export default withStyles(styles)(DashboardItem);
